a2736ce0f096e2de1a6f2dc5be549b5ab50ede87
PC8923 <jihanbhuiyan2020@gmail.com>
2022-08-08 13:10:52 -0400

    added comments

--- car-plot.R
+++ car-plot.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(ggthemes)
 car_plot <- mtcars |>
-  select(mpg, wt) |>
+  select(mpg, wt) |> # we are only going to be graphing the mpg and weight variables
   mutate(tons = wt/2) |>
   ggplot(mapping = aes(x = tons, y = mpg)) +
   geom_point() +
-  geom_smooth(method = "lm", formula = y~x, se  = FALSE) +
+  geom_smooth(method = "lm", formula = y~x, se  = FALSE) + # this function produces a trendline for the scatterplot
   theme_clean() +
   labs(title = "Fuel Efficiency and Weight", subtitle = "Heavier cars are less efficient", x = "Weight (Tons)", y = "MPG", caption = "Henderson and Vellerman (1981)")
 
